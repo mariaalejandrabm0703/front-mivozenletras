8bae0c1146cabe68d33d4443f2055a162e0db8f4
maria.bohorquez <maria.bohorquez@ceiba.com.co>
2023-04-07 10:48:36 -0500

    feature: update component TextForm to register text in page Admin

--- src/components/moleculas/LoginForm.tsx
+++ src/components/moleculas/LoginForm.tsx
@@ -2,13 +2,13 @@ function LoginForm() {
   return (
     <form className="flex flex-col items-center py-2 rounded-lg border border-gray-300">
       <div className="grid grid-cols-2">
-        <div className="p-4">
+        <div className="p-6">
           <div className="flex flex-col w-full mb-6">
             <input
               id="nombre"
               type="text"
               placeholder="Nombre"
-              className="p-2 bg-gray-200 rounded-lg focus:outline-none"
+              className="py-2 px-6 bg-gray-200 rounded-lg focus:outline-none"
               required
             />
           </div>
@@ -17,24 +17,24 @@ function LoginForm() {
               id="email"
               type="email"
               placeholder="Correo electrónico"
-              className="p-2 bg-gray-200 rounded-lg focus:outline-none"
+              className="py-2 px-6 bg-gray-200 rounded-lg focus:outline-none"
               required
             />
           </div>
         </div>
-        <div className="p-4">
+        <div className="p-6">
           <div className="flex flex-col w-full mb-6">
             <input
               id="password"
               type="password"
               placeholder="Contraseña"
-              className="p-2 bg-gray-200 rounded-lg focus:outline-none"
+              className="py-2 px-6 bg-gray-200 rounded-lg focus:outline-none"
               required
             />
           </div>
           <button
             type="submit"
-            className="px-16 py-2 text-white bg-gray-500 rounded-lg font-semibold hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+            className="px-20 py-2 text-white bg-gray-500 rounded-lg font-semibold hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
           >
             Ingresar
           </button>
--- src/components/moleculas/TextForm.tsx
+++ src/components/moleculas/TextForm.tsx
@@ -1,13 +1,18 @@
-
 function TextForm() {
   return (
-    <div>
-      <input
-        className="h-40px text-18px px-10px border border-solid border-gray-300 rounded-l-md"
-        type="text"
-        placeholder="Escribe aquí..."
-      />
-      <button className="bg-blue-500 text-white text-18px h-40px border-none rounded-r-md px-5 cursor-pointer">
+    <div className="flex flex-col items-center py-2 rounded-lg border border-gray-300">
+      <div className="flex flex-col w-full p-4">
+        <textarea
+          id="nombre"
+          placeholder="Publica tus pensamientos ..."
+          className="p-2 bg-gray-200 rounded-lg focus:outline-none"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="px-16 py-2 text-white bg-gray-500 rounded-lg font-semibold hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+      >
         Enviar
       </button>
     </div>
