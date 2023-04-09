334916c5459f3376ee7494190e5e8c824a6da102
maria.bohorquez <maria.bohorquez@ceiba.com.co>
2023-04-09 10:28:45 -0500

    feature: add form to register text

--- src/components/moleculas/TextForm.tsx
+++ src/components/moleculas/TextForm.tsx
@@ -1,20 +1,83 @@
+import { useFormik } from "formik";
+import { toast } from "sonner";
+import * as Yup from "yup";
+
+const URL = "https://mivozenletras.fly.dev/api/v1";
+
+const validationSchema = Yup.object({
+  title: Yup.string().required("El titulo es obligatorio."),
+  description: Yup.string().required("La descripción es obligatorio."),
+});
+
 function TextForm() {
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      description: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          title: values.title,
+          description: values.description,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      fetch(`${URL}/texts/save`, options)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.success === false) {
+            toast.error(data.message);
+            return;
+          }
+          if (Boolean(data) === true) {
+            localStorage.setItem("texts/save", data);
+            toast.success("Texto registrado exitosamente.");
+            formik.resetForm();
+          } else {
+            toast.error("No se ha podido registrar texto.");
+          }
+        });
+    },
+  });
+
   return (
-    <div className="flex flex-col items-center py-2 rounded-lg border border-gray-300">
+    <form
+      onSubmit={formik.handleSubmit}
+      className="flex flex-col items-center py-2 rounded-lg border border-gray-300"
+    >
       <div className="flex flex-col w-full p-4">
         <input
-          id="nombre"
+          id="title"
+          name="title"
           type="text"
           placeholder="Titulo"
           className="py-2 px-2 mb-5 bg-gray-200 rounded-lg focus:outline-none"
-          required
+          onChange={formik.handleChange}
+          value={formik.values.title}
         />
+        {formik.errors.title && formik.touched.title && (
+          <div className="text-red-500 text-xs">{formik.errors.title}</div>
+        )}
         <textarea
-          id="nombre"
+          id="description"
+          name="description"
           placeholder="Publica tus pensamientos ..."
           className="p-2 bg-gray-200 rounded-lg focus:outline-none"
-          required
+          onChange={formik.handleChange}
+          value={formik.values.description}
         />
+        {formik.errors.description && formik.touched.description && (
+          <div className="text-red-500 text-xs">
+            {formik.errors.description}
+          </div>
+        )}
       </div>
       <button
         type="submit"
@@ -22,7 +85,7 @@ function TextForm() {
       >
         Enviar
       </button>
-    </div>
+    </form>
   );
 }
 
