d3876eafda8b103497dfc748394b84c8c4d14474
Maria Bohorquez <65203235+mariaalejandrabm0703@users.noreply.github.com>
2023-07-29 15:11:29 -0500

    Update text.tsx

--- src/components/moleculas/text.tsx
+++ src/components/moleculas/text.tsx
@@ -9,8 +9,8 @@ const Text = ({ title, description, dateTime }: TextTypeProps) => {
     <div className="pt-12 border-t dark:border-gray-700">
       <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row">
         <img
-          src="https://actionaidrecycling.org.uk/wp-content/uploads/2021/01/johann-siemens-EPy0gBJzzZU-unsplash-1208x800.jpg"
-          alt=""
+          src="/pictures/tree1.jpeg"
+          alt="imagen-tree"
           className="self-center flex-shrink-0 w-24 h-24 border rounded-full md:justify-self-start dark:bg-gray-500 dark:border-gray-700"
         />
         <div className="flex flex-col">
