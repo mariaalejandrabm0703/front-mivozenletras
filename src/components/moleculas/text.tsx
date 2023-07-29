2ece0a0f9281f3e0ef95788c999f834fc2bed173
Maria Bohorquez <mariaalejandrabm0703@gmail.com>
2023-07-29 17:25:48 -0500

    fix: show image

--- src/components/moleculas/text.tsx
+++ src/components/moleculas/text.tsx
@@ -9,7 +9,7 @@ const Text = ({ title, description, dateTime }: TextTypeProps) => {
     <div className="pt-12 border-t dark:border-gray-700">
       <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row">
         <img
-          src="/pictures/tree1.jpeg"
+          src="./pictures/tree1.jpeg"
           alt="imagen-tree"
           className="self-center flex-shrink-0 w-24 h-24 border rounded-full md:justify-self-start dark:bg-gray-500 dark:border-gray-700"
         />
